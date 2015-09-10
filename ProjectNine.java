4696bd0e2e3f57d002d0caf4eb52b03f248f854e
AniahLiepard <aniah.garcia@techhigh.us>
2015-09-10 10:07:48 -0400

    Switch Statement Project
    https://www.thenewboston.com/videos.php?cat=31&amp;video=17977

---
+++ ProjectNine.java
@@ -0,0 +1,20 @@
+public class ProjectNine {
+	public static void main(String args[]){
+		int dog = 3;
+		
+		switch (dog){
+		case 1:
+			System.out.println("You have a Bloodhound.");
+			break;
+		case 2:
+			System.out.println("You have a Doberman.");
+			break;
+		case 3:
+			System.out.println("You have a Great Dane.");
+			break;
+		default:
+			System.out.println("You have a Rottweiler. How unfortunate.");
+			break;
+		}
+	}
+}
